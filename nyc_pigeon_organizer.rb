a94d441296b8ffaa710d06e05cff44b49a6ebc2e
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 05:46:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,20 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  pigeon_list = {}
-  data.each do {|color, gender, lives|}
   
-    pigeon_list[:name => pigeon_data[:gender][:male]]
+  results = data.each_with_oject({}) do |(key, value), pigeon_list|
+  
+  value.each do |inner_key, names|
+    names.each do |name|
+      if !pigeon_list[name]
+        pigeon_list[name] = {}
+      end
+      if !pigeon_list[name][key]
+        !pigeon_list[name][key] = []
+      end
+      pigeon_list[name][key].push(inner_key.to_s)
+    end
+  end
+  
+    #pigeon_list[:name => pigeon_data[:gender][:male]]
   end
 end
 
