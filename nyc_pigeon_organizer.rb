9c967d7436a013885695c2996e04c9b98dcef423
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 05:49:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,17 @@
 def nyc_pigeon_organizer(data)
   
-  results = data.each_with_oject({}) do |(key, value), pigeon_list|
-  
-  value.each do |inner_key, names|
-    names.each do |name|
-      if !pigeon_list[name]
-        pigeon_list[name] = {}
+  results = data.each_with_object({}) do |(key, value), pigeon_list|
+    value.each do |inner_key, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key]
+          !pigeon_list[name][key] = []
+        end
+        pigeon_list[name][key].push(inner_key.to_s)
       end
-      if !pigeon_list[name][key]
-        !pigeon_list[name][key] = []
-      end
-      pigeon_list[name][key].push(inner_key.to_s)
     end
-  end
-  
-    #pigeon_list[:name => pigeon_data[:gender][:male]]
   end
 end
 
@@ -26,3 +23,4 @@ end
 #if name equals 
 #  then get color , gender and lives
 #end 
+ #pigeon_list[:name => pigeon_data[:gender][:male]]
