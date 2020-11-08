e68553249e5266a841bc305561cdefb18f765d89
Sin Wildy <sw.wildy@gmail.com>
2020-11-08 05:07:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do {|color, gender, lives|}
+  
+    pigeon_list[:name => pigeon_data[:gender][:male]]
+  end
 end
+
+
+
+#create a new empty hash
+#go thry pigeon data
+#create keys under names
+#if name equals 
+#  then get color , gender and lives
+#end 
